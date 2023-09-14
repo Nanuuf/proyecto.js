b574b6a013eaab941f80ee8f755d335290550317
Nahir Fernandez <nahirmfernandez0@gmail.com>
2023-09-14 20:44:11 -0300

    arreglos tercer entrega

--- JS/javasc/turnos.js
+++ JS/javasc/turnos.js
@@ -22,11 +22,50 @@ function registrarPaciente() {
 
     // Mostrar el modal de confirmación
     mostrarModal('Paciente registrado exitosamente.');
-
+    
     // Ocultar el formulario después de guardar la información
     const formulario = document.getElementById('formularioRegistro'); 
     formulario.style.display = 'none';
 
+}
+
+
+
+function compararNombre() {
+    // Obtener el nombre ingresado por el usuario
+    const nombreIngresado = document.getElementById('nombrePaciente').value;
+
+    // Obtener el nombre registrado en el localStorage
+    const pacienteJSON = localStorage.getItem("paciente");
+
+    
+    const selectHorario = document.getElementById('horario');
+    selectHorario.addEventListener('change', function() {
+        actualizarTurnoSeleccionado();
+    });
+
+    if (pacienteJSON) {
+        // Parsear el objeto JSON almacenado
+        const paciente = JSON.parse(pacienteJSON);
+
+        // Comparar el nombre ingresado con el nombre registrado
+        if (nombreIngresado === paciente.nombre) {
+            const mensaje = `¡Bienvenido de nuevo, ${nombreIngresado}!`;
+            mostrarModal(mensaje);
+
+            // Lógica para obtener el turno seleccionado (debes implementar esta parte)
+            const diaSeleccionado = document.getElementById('dia').value;
+            const horarioSeleccionado = selectHorario.options[selectHorario.selectedIndex].text;
+            const turnoSeleccionado = `Día: ${diaSeleccionado}, Horario: ${horarioSeleccionado}`;
+            mostrarTurnoSeleccionado(turnoSeleccionado);
+        } else {
+            const mensaje = `El nombre ${nombreIngresado} no coincide con el nombre registrado.`;
+            mostrarModalError(mensaje);
+        }
+    } else {
+        const mensaje = `No hay un paciente registrado. Por favor, regístrese.`;
+        mostrarModalError(mensaje);
+    }
 }
 
 function mostrarModal(mensaje) {
@@ -36,89 +75,30 @@ function mostrarModal(mensaje) {
     modal.show();
 }
 
+function mostrarModalError(mensaje) {
+    const mensajeError = document.getElementById('mensajeError');
+    mensajeError.textContent = mensaje;
+    const modal = new bootstrap.Modal(document.getElementById('errorModal'));
+    modal.show();
+}
+
+function mostrarTurnoSeleccionado(turno) {
+    const mensajeTurno = document.getElementById('mensajeTurno');
+    mensajeTurno.textContent = turno;
+    const modal = new bootstrap.Modal(document.getElementById('turnoModal'));
+    modal.show();
+}
+
 
 // Evento click del botón "Reservar"
 document.getElementById('reservarBtn').addEventListener('click', function () {
-    const nombrePaciente = document.getElementById('nombre').value;
-    const dia = document.getElementById('dia').value;
-    const horario = document.getElementById('horario').value;
-    
-    if (verificarRegistro(nombrePaciente)) {
-        const mensaje = `¡Turno confirmado para ${nombrePaciente} el día ${dia} a las ${horario}!`;
-        document.getElementById('confirmacionTexto').textContent = mensaje;
-        mostrarModalConfirmacion();
-    } else {
-        const mensaje = `El nombre ${nombrePaciente} no está registrado. Por favor, regístrese.`;
-        document.getElementById('advertenciaTexto').textContent = mensaje;
-        mostrarModalAdvertencia();
-    }
+    compararNombre();
 });
 
 
 
-//CODIGO COMENTADO XQ NO ESTA TERMINADO
-// class ControladorPacientes {
-//     constructor() {
-//         this.pacientes = [];
-//         this.turnosOcupados = {};
-//     }
-//     agregarPaciente(paciente) {
-//         this.pacientes.push(paciente);
-//     }
-//     buscarPaciente(nombre) {
-//         return this.pacientes.find(paciente => paciente.nombreCompleto.toLowerCase() === nombre.toLowerCase());
-
-//     }
-//     pasarNombreMayuscula(paciente){
-//         return {
-//             ...paciente,
-//             nombreCompleto: paciente.nombreCompleto.toUpperCase()
-//         }
-//     }
-//     filtrarPacientesPorSesion(sesion) {
-//         return this.pacientes.filter(paciente => paciente.sesion.toLowerCase() === sesion.toLowerCase());
-//     }
-
-//     //para calcular años del paciente, aun trabajando en esta funcion
-//     calcularEdad(fechaNacimiento){
-//         const ahora = new Date();
-//         const nacimiento = new Date(fechaNacimiento);
-//         let edad = ahora.getFullYear() - nacimiento.getFullYear();
-        
-//         if (ahora.getMonth() < nacimiento.getMonth() || (ahora.getMonth() === nacimiento.getMonth() && ahora.getDate() < nacimiento.getDate()  )) {
-//             edad--; //para completar la edad si aun no cumplio años
-//         }
-//         return edad; 
-//     }
 
 
-
-//manipulación de datos internos
-//funcion para que el usuario pueda cambiar info
-function modificarDatosPacientes(paciente){
-    console.log("Datos actuales del paciente: "); 
-    for (const propiedad in paciente) {
-        console.log(`${propiedad}: ${paciente[propiedad]}`);
-    }
-    const actualizar = prompt("Desea actualizar sus datos? (si/no)").toLowerCase();
-    if (actualizar === "si") {
-        const nuevaFechaNacimiento = new Date(prompt("Nueva fecha de nacimiento: "));
-        paciente.fechaNacimiento = nuevaFechaNacimiento;
-
-        console.log("Datos actualizados:");
-        for (const propiedad in paciente) {
-            console.log(`${propiedad}: ${paciente[propiedad]}`);
-        }
-    }
-}
-
-function cancelarTurno(paciente){
-    const confirmacion = prompt("¿Está seguro de que desea cancelar su turno? (si/no)").toLowerCase();
-    if (confirmacion === "si") {
-        // Código para eliminar el turno
-        console.log("Su turno ha sido cancelado.");
-    }
-}
 
 
 //mostrar formulario cuando selecciona NO
@@ -146,7 +126,3 @@ checkSi.addEventListener('change', function() {
         formulario.style.display = 'none';
     }
 });
-
-
-
-
